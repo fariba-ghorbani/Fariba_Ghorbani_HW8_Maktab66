fcfd39889234d2778c2a944c66c0d6708d64c70e
Fariba <fariba.ghorbani2127@gmail.com>
2021-12-30 11:07:59 +0330

    HW8 is uploaded

--- 3/main.js
+++ 3/main.js
@@ -1,13 +1,9 @@
-// const hamburgerToggle = document.getElementsByClassName("hamburger")[0];
-// const logoLabel = document.getElementsByClassName("logo")[0];
-// const allLinks = document.getElementsByClassName("navbar-items")[0];
-// const navigationBar = document.getElementsByClassName("navbar")[0];
+const hamburgerToggle = document.getElementsByClassName("resp-nav")[0];
+const allLinks = document.getElementsByClassName("links")[0];
 
-// hamburgerToggle.addEventListener('click', () => {
-//     allLinks.classList.toggle('active');
-//     logoLabel.classList.toggle('active');
-//     navigationBar.classList.toggle('active');
-// })
+hamburgerToggle.addEventListener('click', () => {
+    allLinks.classList.toggle('active');
+})
 
 const dressIcon = document.getElementsByClassName("dress")[0];
 const bootIcon = document.getElementsByClassName("boot")[0];
@@ -24,45 +20,75 @@ const skinContent = document.getElementsByClassName("skin")[0];
 
 dressIcon.addEventListener('click', () => {
     fashionContent.style.opacity = 0;
+    fashionContent.style.position = 'absolute'
     clothesContent.style.opacity = 1;
+    clothesContent.style.position = 'relative';
     cosmeticsContent.style.opacity = 0;
+    cosmeticsContent.style.position = 'absolute';
     shoesContent.style.opacity = 0;
+    shoesContent.style.position = 'absolute';
     accessoriesContent.style.opacity = 0;
+    accessoriesContent.style.position = 'absolute';
     skinContent.style.opacity = 0;
+    skinContent.style.position = 'absolute'
 })
 
 bootIcon.addEventListener('click', () => {
     fashionContent.style.opacity = 0;
+    fashionContent.style.position = 'absolute';
     clothesContent.style.opacity = 0;
+    clothesContent.style.position = 'absolute'
     cosmeticsContent.style.opacity = 0;
+    cosmeticsContent.style.position = 'absolute';
     shoesContent.style.opacity = 1;
+    shoesContent.style.position = 'relative';
     accessoriesContent.style.opacity = 0;
+    accessoriesContent.style.position = 'absolute';
     skinContent.style.opacity = 0;
+    skinContent.style.position = 'absolute'
 })
 
 lipstickIcon.addEventListener('click', () => {
     fashionContent.style.opacity = 0;
+    fashionContent.style.position = 'absolute';
     clothesContent.style.opacity = 0;
+    clothesContent.style.position = 'absolute';
     cosmeticsContent.style.opacity = 1;
+    cosmeticsContent.style.position = 'relative';
     shoesContent.style.opacity = 0;
+    shoesContent.style.position = 'absolute';
     accessoriesContent.style.opacity = 0;
+    accessoriesContent.style.position = 'absolute';
     skinContent.style.opacity = 0;
+    skinContent.style.position = 'absolute'
 })
 
 lotionIcon.addEventListener('click', () => {
     fashionContent.style.opacity = 0;
+    fashionContent.style.position = 'absolute';
     clothesContent.style.opacity = 0;
+    clothesContent.style.position = 'absolute';
     cosmeticsContent.style.opacity = 0;
+    cosmeticsContent.style.position = 'absolute';
     shoesContent.style.opacity = 0;
+    shoesContent.style.position = 'absolute';
     accessoriesContent.style.opacity = 0;
+    accessoriesContent.style.position = 'absolute';
     skinContent.style.opacity = 1;
+    skinContent.style.position = 'relative';
 })
 
 glassIcon.addEventListener('click', () => {
     fashionContent.style.opacity = 0;
+    fashionContent.style.position = 'absolute';
     clothesContent.style.opacity = 0;
+    clothesContent.style.position = 'absolute';
     cosmeticsContent.style.opacity = 0;
+    cosmeticsContent.style.position = 'absolute';
     shoesContent.style.opacity = 0;
+    shoesContent.style.position = 'absolute';
     accessoriesContent.style.opacity = 1;
+    accessoriesContent.style.position = 'relative';
     skinContent.style.opacity = 0;
+    skinContent.style.position = 'absolute';
 })
